129ae828162a470c1055f36c36a4dd48656fd053
千迦 <qianjia.work@gmail.com>
2024-02-20 13:31:19 +0800

    feat(後端): check-auth 修改邏輯、整理程式碼
    1. 移除只有本人可以操作的權限方法
    2. 整理程式碼

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,11 +1,10 @@
 const jwt = require("jsonwebtoken")
 const HttpError = require("../models/http-error")
-// 權限檢查分成五種：
-// 1. 取，沒有沒關係(關係到給的資料詳細度)
+// 權限檢查分成四種：
+// 1. 取，沒有沒關係(關係到給的資料詳細度)，驗證失敗跳過就好
 // 2. 取，要是會員或admin才能操作
 // 3. 取，要是本人或是admin才能操作
-// 4. 取，只能是本人才能操作
-// 5. 取，只有admin才能操作
+// 4. 取，只有admin才能操作
 
 const getAuth = (req, res, next) => {
 	if (req.method === "OPTIONS") {
@@ -13,24 +12,26 @@ const getAuth = (req, res, next) => {
 	}
 
 	const token = req.headers.authorization?.split(" ")[1]
-
 	if (!token) {
 		return next()
 	}
 
+	let decodedToken
 	try {
-		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
-		req.authData = {
-			id: decodedToken.id,
-			email: decodedToken.email,
-			role: decodedToken.role
-		}
-		return next()
+		decodedToken = jwt.verify(code, process.env.JWT_KEY)
 	} catch (error) {
-		console.log(error)
-		return next(new HttpError("Authentication failed", 401))
+		return next()
 	}
+
+	req.authData = {
+		id: decodedToken.id,
+		email: decodedToken.email,
+		role: decodedToken.role
+	}
+
+	return next()
 }
+
 const checkAuthMemberOrAdmin = (req, res, next) => {
 	if (req.method === "OPTIONS") {
 		return next()
@@ -41,45 +42,17 @@ const checkAuthMemberOrAdmin = (req, res, next) => {
 		return next(new HttpError("Unauthorized,", 401))
 	}
 
-	try {
-		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
-
-		if (decodedToken.role !== "user" && decodedToken.role !== "admin") {
-			return next(new HttpError("Permission denied,", 403))
-		}
-
-		req.authData = {
-			id: decodedToken.id,
-			email: decodedToken.email,
-			role: decodedToken.role
-		}
-
-		return next()
-	} catch (error) {
-		console.log(error)
-		return next(new HttpError("Authentication failed", 401))
-	}
-}
-const checkAuthSelfOrAdmin = (req, res, next) => {
-	if (req.method === "OPTIONS") {
-		return next()
-	}
-	let token = req.headers.authorization?.split(" ")[1]
-
-	if (!token) {
-		return next(new HttpError("Unauthorized,", 401))
-	}
-
 	let decodedToken
 	try {
 		decodedToken = jwt.verify(token, process.env.JWT_KEY)
 	} catch (error) {
-		return next(
-			new HttpError(error.message || "Authentication failed", 401)
-		)
+		if (error.message === "jwt expired") {
+			return next(new HttpError("Authentication token expired", 401))
+		}
+		return next(new HttpError("Authentication failed", 401))
 	}
 
-	if (req.params.id !== decodedToken.id && decodedToken.role !== "admin") {
+	if (decodedToken.role !== "user" && decodedToken.role !== "admin") {
 		return next(new HttpError("Permission denied,", 403))
 	}
 
@@ -91,7 +64,8 @@ const checkAuthSelfOrAdmin = (req, res, next) => {
 
 	return next()
 }
-const checkAuthSelf = (req, res, next) => {
+
+const checkAuthSelfOrAdmin = (req, res, next) => {
 	if (req.method === "OPTIONS") {
 		return next()
 	}
@@ -101,25 +75,29 @@ const checkAuthSelf = (req, res, next) => {
 		return next(new HttpError("Unauthorized,", 401))
 	}
 
+	let decodedToken
 	try {
-		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
-
-		if (req.params.id !== decodedToken.id) {
-			return next(new HttpError("Permission denied,", 403))
-		}
-
-		req.authData = {
-			id: decodedToken.id,
-			email: decodedToken.email,
-			role: decodedToken.role
-		}
-
-		return next()
+		decodedToken = jwt.verify(token, process.env.JWT_KEY)
 	} catch (error) {
-		console.log(error)
+		if (error.message === "jwt expired") {
+			return next(new HttpError("Authentication token expired", 401))
+		}
 		return next(new HttpError("Authentication failed", 401))
 	}
+
+	if (decodedToken.id !== req.params.id && decodedToken.role !== "admin") {
+		return next(new HttpError("Permission denied,", 403))
+	}
+
+	req.authData = {
+		id: decodedToken.id,
+		email: decodedToken.email,
+		role: decodedToken.role
+	}
+
+	return next()
 }
+
 const checkAuthAdmin = (req, res, next) => {
 	if (req.method === "OPTIONS") {
 		return next()
@@ -130,28 +108,30 @@ const checkAuthAdmin = (req, res, next) => {
 		return next(new HttpError("Unauthorized,", 401))
 	}
 
+	let decodedToken
 	try {
-		const decodedToken = jwt.verify(token, process.env.JWT_KEY)
-
-		if (decodedToken.role !== "admin") {
-			return next(new HttpError("Permission denied,", 403))
-		}
-
-		req.authData = {
-			id: decodedToken.id,
-			email: decodedToken.email,
-			role: decodedToken.role
-		}
-
-		return next()
+		decodedToken = jwt.verify(token, process.env.JWT_KEY)
 	} catch (error) {
-		console.log(error)
+		if (error.message === "jwt expired") {
+			return next(new HttpError("Authentication token expired", 401))
+		}
 		return next(new HttpError("Authentication failed", 401))
 	}
+
+	if (decodedToken.role !== "admin") {
+		return next(new HttpError("Permission denied,", 403))
+	}
+
+	req.authData = {
+		id: decodedToken.id,
+		email: decodedToken.email,
+		role: decodedToken.role
+	}
+
+	return next()
 }
 
 module.exports.getAuth = getAuth
 module.exports.checkAuthMemberOrAdmin = checkAuthMemberOrAdmin
 module.exports.checkAuthSelfOrAdmin = checkAuthSelfOrAdmin
-module.exports.checkAuthSelf = checkAuthSelf
 module.exports.checkAuthAdmin = checkAuthAdmin
