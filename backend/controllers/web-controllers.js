cf82ea1c1893e5a104836679bd5016bba04b4461
千迦 <qianjia.work@gmail.com>
2024-02-20 19:08:51 +0800

    feat(後端): 新增網站info、聯絡我 API

--- backend/controllers/web-controllers.js
+++ backend/controllers/web-controllers.js
@@ -1,7 +1,49 @@
 const HttpError = require("../models/http-error")
 
-const getInfo = async (req, res, next) => {}
-const postContact = async (req, res, next) => {}
+const Web = require("../models/web")
+const Contact = require("../models/contact")
+
+const getInfo = async (req, res, next) => {
+	let webInfo
+	try {
+		webInfo = await Web.findOne({ name: "info" })
+	} catch (error) {
+		return next(
+			new HttpError(
+				"Fetching web info failed, please try again later.",
+				500
+			)
+		)
+	}
+	res.json({
+		data: webInfo.toObject({ getters: true })
+	})
+}
+const postContact = async (req, res, next) => {
+	const { title, content, email } = req.body
+	let contact
+	contact = new Contact({
+		title,
+		content,
+		email
+	})
+
+	try {
+		await contact.save()
+	} catch (error) {
+		console.log(error)
+		return next(
+			new HttpError(
+				"Fetching contact failed, please try again later.",
+				500
+			)
+		)
+	}
+
+	res.json({
+		massage: "已發送訊息！"
+	})
+}
 
 exports.getInfo = getInfo
 exports.postContact = postContact
--- backend/routes/web-routes.js
+++ backend/routes/web-routes.js
@@ -1,8 +1,20 @@
 const express = require("express")
-const webControllers = require("../controllers/web-controllers")
 const router = express.Router()
+const { check } = require("express-validator")
+const { nextValidatorError } = require("../middleware/next-validator-error")
+
+const webControllers = require("../controllers/web-controllers")
 
 router.get("/info", webControllers.getInfo)
-router.post("/contact", webControllers.postContact)
+router.post(
+	"/contact",
+	[
+		check("title").not().isEmpty().withMessage("Title is required"),
+		check("content").not().isEmpty().withMessage("Content is required"),
+		check("email").optional().isEmail().withMessage("Invalid email")
+	],
+	nextValidatorError,
+	webControllers.postContact
+)
 
 module.exports = router
