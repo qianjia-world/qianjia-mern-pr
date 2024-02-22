58534ca122691e34de3ac4125a3542551ad6d724
千迦 <qianjia.work@gmail.com>
2024-02-22 12:30:49 +0800

    feat(前端): 安裝tailwind prettier插件
    1. npm install -D prettier prettier-plugin-tailwindcss
    2. 新增 prettirr config
    3. 重新儲存各個檔案來變更排版

--- frontend/app/ws/page.js
+++ frontend/app/ws/page.js
@@ -1,62 +1,62 @@
-"use client"
-import React, { useState } from "react"
+"use client";
+import React, { useState } from "react";
 
-let ws
+let ws;
 function Page() {
-	const [msg, setMsg] = useState(null)
+  const [msg, setMsg] = useState(null);
 
-	function connect() {
-		console.log("[click connect]")
-		// Create WebSocket connection
-		ws = new WebSocket("ws://localhost:8080")
-		// 在開啟連線時執行
-		ws.onopen = () => console.log("[open connection]")
-		ws.onmessage = (event) => console.log("[send message]", event.data)
-	}
+  function connect() {
+    console.log("[click connect]");
+    // Create WebSocket connection
+    ws = new WebSocket("ws://localhost:8080");
+    // 在開啟連線時執行
+    ws.onopen = () => console.log("[open connection]");
+    ws.onmessage = (event) => console.log("[send message]", event.data);
+  }
 
-	function disconnect() {
-		console.log("[click disconnect]")
-		ws.close()
-		// 在關閉連線時執行
-		ws.onclose = () => console.log("[close connection]")
-	}
+  function disconnect() {
+    console.log("[click disconnect]");
+    ws.close();
+    // 在關閉連線時執行
+    ws.onclose = () => console.log("[close connection]");
+  }
 
-	function sendMessage(msg) {
-		console.log("[click send]")
-		ws.send(msg)
-	}
-	return (
-		<div className='bg-white w-full h-screen text-black'>
-			<h1>WS</h1>
-			<button
-				className='py-2 px-4 m-1 bg-yellow-200 border-2 rounded-md'
-				onClick={connect}
-			>
-				Connect
-			</button>
-			<button
-				className='py-2 px-4 m-1 bg-yellow-200 border-2 rounded-md'
-				onClick={disconnect}
-			>
-				Disconnect
-			</button>
-			<input
-				className='border-2 rounded-md h-[40px]'
-				type='text'
-				id='sendMsg'
-				onChange={(e) => {
-					console.log(e.target.value)
-					setMsg(e.target.value)
-				}}
-			/>
-			<button
-				className='py-2 px-4 m-1 bg-yellow-200 border-2 rounded-md'
-				onClick={sendMessage}
-			>
-				Send
-			</button>
-		</div>
-	)
+  function sendMessage(msg) {
+    console.log("[click send]");
+    ws.send(msg);
+  }
+  return (
+    <div className="h-screen w-full bg-white text-black">
+      <h1>WS</h1>
+      <button
+        className="m-1 rounded-md border-2 bg-yellow-200 px-4 py-2"
+        onClick={connect}
+      >
+        Connect
+      </button>
+      <button
+        className="m-1 rounded-md border-2 bg-yellow-200 px-4 py-2"
+        onClick={disconnect}
+      >
+        Disconnect
+      </button>
+      <input
+        className="h-[40px] rounded-md border-2"
+        type="text"
+        id="sendMsg"
+        onChange={(e) => {
+          console.log(e.target.value);
+          setMsg(e.target.value);
+        }}
+      />
+      <button
+        className="m-1 rounded-md border-2 bg-yellow-200 px-4 py-2"
+        onClick={sendMessage}
+      >
+        Send
+      </button>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
