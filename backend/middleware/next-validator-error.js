42e605a03fe43e0b11fe7a5ecc3b70f345738acd
千迦 <qianjia.work@gmail.com>
2024-03-03 14:33:17 +0800

    feat(後端): 將error訊息以字串連接

--- backend/middleware/next-validator-error.js
+++ backend/middleware/next-validator-error.js
@@ -2,11 +2,16 @@ const { validationResult } = require("express-validator")
 const nextValidatorError = (req, res, next) => {
 	const errors = validationResult(req)
 	if (!errors.isEmpty()) {
+		let message = errors
+			.array()
+			.map((error) => error.msg)
+			.join(",")
 		res.status(400).json({
-			message: errors.array() || "An unknown error occurred!"
+			message: message || "An unknown error occurred!"
 		})
+	} else {
+		next()
 	}
-	next()
 }
 
 exports.nextValidatorError = nextValidatorError
