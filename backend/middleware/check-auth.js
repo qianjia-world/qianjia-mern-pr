e2b862c16b0fe2c9518f01760ebf2f72878ba7ff
千迦 <qianjia.work@gmail.com>
2024-03-12 02:32:37 +0800

    feat(後端): 修改getAuth的邏輯，如果有帶token的話，驗證失敗也要跳出來

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 const HttpError = require("../models/http-error")
 // 權限檢查分成四種：
-// 1. 取，沒有沒關係(關係到給的資料詳細度)，驗證失敗跳過就好
+// 1. 取，沒有沒關係(關係到給的資料詳細度)，但decoded驗證失敗要跳出來。
 // 2. 取，要是會員或admin才能操作
 // 3. 取，要是本人或是admin才能操作
 // 4. 取，只有admin才能操作
@@ -20,7 +20,10 @@ const getAuth = (req, res, next) => {
 	try {
 		decodedToken = jwt.verify(token, process.env.JWT_KEY)
 	} catch (error) {
-		return next()
+		if (error.message === "jwt expired") {
+			return next(new HttpError("Authentication code expired", 401))
+		}
+		return next(new HttpError("Authentication failed", 401))
 	}
 
 	req.authData = {
