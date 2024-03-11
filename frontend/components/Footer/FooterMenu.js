de673e82585e2ef3d86c6adb738145b907faa003
千迦 <qianjia.work@gmail.com>
2024-03-12 02:44:22 +0800

    fix(前端): menu not show member auth in footer

--- frontend/components/Footer/FooterMenu.js
+++ frontend/components/Footer/FooterMenu.js
@@ -4,11 +4,12 @@ import Link from "next/link";
 import Logo from "@/components/shared/Logo";
 
 export default function FooterMenu() {
+  const data = MenuData.slice(0, -2);
   return (
     <Wrap className="border-y-2 border-primary">
       <div className="grid grid-cols-1 gap-y-14  px-5 pb-10 pt-5 sm:px-16 sm:py-14 md:grid-cols-2 lg:px-32">
         <ul className="grid grid-flow-col grid-rows-5 gap-x-1 gap-y-4 border-b-2   border-primary p-1 text-lg sm:grid-rows-4 sm:gap-y-3 ">
-          {MenuData.map((item) => {
+          {data.map((item) => {
             return (
               <li
                 key={item.title}
